098951cc16ee8f4bcfb759a4c2573250cc71c44c
DannyDaniel-9 <3dldadaniel26@gmail.com>
2025-10-20 10:20:21 +0200

    subida de tareas

---
+++ MostrarTrump/script3.js
@@ -0,0 +1,11 @@
+function ocultaTrump() {
+    var trump =document.getElementById("trump")
+    trump.style.opacity= 0;
+
+}
+
+function mostrarTrump() {
+    var trump =document.getElementById("trump")
+    trump.style.opacity= 1;
+
+}
